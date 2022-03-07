240d00248b507591064f41a52787b718f3f59b3b
hhs44 <hhs44@qq.com>
2022-03-08 00:06:21 +0800

    更新blog 和 新增top文章，实时更新进展

--- hugo/models.py
+++ hugo/models.py
@@ -5,14 +5,14 @@ from django.db import models
 # Create your models here.
 from mdeditor.fields import MDTextField
 
-from hugo.utils.settings import POST_PATH, ABOUT_PATH, ABOUT_TOP, IMAGE_PATH, POST_PAGE_PATH
+from hugo.utils.settings import POST_PATH, ABOUT_PATH, ABOUT_TOP, IMAGE_PATH, POST_PAGE_PATH, INDEX_IMAGE_PATH
 
 
 class Base(models.Model):
     title = models.CharField("标题", max_length=255, default="")
     description = models.CharField("简介", max_length=255, blank=True, default="")
     date = models.DateTimeField("时间", default="")
-    image = models.CharField("封面", max_length=255, blank=True, default="")
+    image = models.ImageField("封面", blank=True, default="")
     tags = models.CharField("标签", max_length=255, blank=True, default="")
     categories = models.CharField("分类", max_length=255, blank=True, default="")
     series = models.CharField("series", max_length=255, blank=True, default='"Themes Guide"')
@@ -39,14 +39,16 @@ class Blog(Base):
         categories = self.cover(self.categories)
         tags = self.cover(self.tags)
         content = self.content
+        os.system("xcopy /y %s %s" % (INDEX_IMAGE_PATH + "\\" + str(self.image), POST_PATH + '\\"' + self.title + '"'))
         if not os.path.exists(POST_PATH + "/" + self.title):
             os.makedirs(POST_PATH + "/" + self.title)
-        image_re = r'![[].*?[]][(](.*?)[)]'
+        image_re = r'![\[].*?[]][(](.*?)[)]'
         ls = re.findall(image_re, self.content, re.M)
         for i in ls:
             image_name = i.split("\\")[-1]
+            print(i, image_name)
             os.system("xcopy /y %s %s" % (IMAGE_PATH + "\\" + image_name, POST_PATH + '\\"' + self.title + '"'))
-            content = self.content.replace(i, image_name)
+            content = content.replace(i, image_name)
         with open("%s\\%s\\index.md" % (POST_PATH, self.title), mode="w", encoding='utf-8') as f:
             f.write('+++\ntitle="%s"\n' % self.title)
             f.write('description="%s"\n' % self.description)
@@ -72,15 +74,16 @@ class About(Base):
 
     def save(self, *args, **kwargs):
         content = self.content
+        os.system("xcopy /y %s %s" % (INDEX_IMAGE_PATH + "\\" + str(self.image), POST_PATH + '\\"' + self.title + '"'))
         aliases = ""
         for i in self.aliases.split(","):
             aliases += " - %s\n" % i
-        image_re = r'![[].*?[]][(](.*?)[)]'
+        image_re = r'![\[].*?[]][(](.*?)[)]'
         ls = re.findall(image_re, self.content, re.M)
         for i in ls:
             image_name = i.split("\\")[-1]
             os.system("xcopy /y %s %s" % (IMAGE_PATH + "\\" + image_name, POST_PAGE_PATH + "\\'" + self.title + "'"))
-            content = self.content.replace(i, image_name)
+            content = content.replace(i, image_name)
         with open(ABOUT_PATH, mode="w", encoding='utf-8') as f:
             f.write(ABOUT_TOP % (
                 self.title, self.description, self.date, aliases, self.md_date))
--- hugo/utils/settings.py
+++ hugo/utils/settings.py
@@ -3,6 +3,7 @@ POST_PATH = "F:\\newblog\\content\\post"
 POST_PAGE_PATH = "F:\\newblog\\content\\page"
 ABOUT_PATH = "F:\\newblog\\content\\page\\about\\index.md"
 IMAGE_PATH = "F:\\newproject\\web\\djangoProject\\uploads\\editor"
+INDEX_IMAGE_PATH = "F:\\newproject\\web\\djangoProject\\uploads"
 ABOUT_TOP = '---\ntitle: %s\n' \
             'description: %s\n' \
             'date: %s\n' \
